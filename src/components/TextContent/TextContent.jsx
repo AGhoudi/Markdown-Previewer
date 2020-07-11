4852dd260c7dff0b7f8d3dca91a63f098a412f50
Link1986 <ali.ghoudi@gmail.com>
2020-07-11 20:24:34 +0200

    modified title and added two boxes, one for editor and one for previewer

---
+++ src/components/TextContent/TextContent.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './TextContent.css';
+
+class TextContent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: ''
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(e) {
+        const textValue = e.target.value;
+        this.setState({
+            text: textValue
+        })
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <div className="editorBox">
+                    <h2 className="editorTitle">&#10002; Editor</h2>
+                    <textarea className="editor" onChange={this.handleChange} name="text"></textarea>
+                </div>
+                <div className="editorBox">
+                    <h2 className="editorTitle">&#10143; Previewer</h2>
+                    <p className="editor text" >{this.state.text}</p>    
+                </div>               
+            </React.Fragment>
+        );
+    }
+}
+
+export default TextContent;
