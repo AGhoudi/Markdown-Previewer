3ea94b2acf76006a953f2c5b1bbe89e51cd3b7cf
Link1986 <ali.ghoudi@gmail.com>
2020-07-15 21:21:47 +0200

    added react-markdown library and worked on previewer

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
         
-      <h1>Markdown Previewer</h1>
+      <h1 className="title">Markdown Previewer</h1>
       <TextContent />
     </div>
   );
--- src/components/TextContent/TextContent.jsx
+++ src/components/TextContent/TextContent.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import Store from '../Store/Store'
 import './TextContent.css';
+import ReactMarkdown from 'react-markdown';
 
 class TextContent extends React.Component {
     constructor(props) {
         super(props);
-        const store = Store.map(store => store );
+        
         this.state = {
-            text: store
+            text: ''
         }
         this.handleChange = this.handleChange.bind(this);
     }
@@ -19,16 +19,15 @@ class TextContent extends React.Component {
     }
 
     render() {
-        
         return (
-            <React.Fragment>
+            <React.Fragment>                
                 <div className="editorBox">
                     <h2 className="editorTitle">&#10002; Editor</h2>
-                    <textarea className="editor" onChange={this.handleChange} name="text" value={this.state.text}></textarea>
+        <textarea className="editor" onChange={this.handleChange} name="text" value={this.state.text} ></textarea>
                 </div>
                 <div className="editorBox">
                     <h2 className="editorTitle">&#10143; Previewer</h2>
-                    <p className="editor text" ><pre>{this.state.text}</pre></p>    
+                    <div className="editor text"><ReactMarkdown source={this.state.text} /></div>    
                 </div>               
             </React.Fragment>
         );
