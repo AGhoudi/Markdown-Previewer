87cba1d260f6c6f7fefe89614c7ceb8f73b50a8c
Link1986 <ali.ghoudi@gmail.com>
2020-07-21 18:18:56 +0200

    Updates

---
+++ precache-manifest.d2c725fe8d8a3dd8cf4d646a5ae21b45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "630dae58ffdd2284ac06b4f64c118678",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "280848520b61d26bd464",
+    "url": "/markdown-previewer/static/css/main.862bae3c.chunk.css"
+  },
+  {
+    "revision": "50a1ed555b2560a22f66",
+    "url": "/markdown-previewer/static/js/2.af8db46b.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/markdown-previewer/static/js/2.af8db46b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "280848520b61d26bd464",
+    "url": "/markdown-previewer/static/js/main.d4952223.chunk.js"
+  },
+  {
+    "revision": "380a9cf99d31db8da525",
+    "url": "/markdown-previewer/static/js/runtime-main.4fd0b226.js"
+  }
+]);
